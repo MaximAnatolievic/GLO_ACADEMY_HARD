54200b71c06d6497b75eac78e0fa212d7befc965
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-18 14:15:39 +0300

    Выполнил 4 усложнённое

--- scripts/script.js
+++ scripts/script.js
@@ -1,68 +1,18 @@
-//Первое задание
-let lang = prompt('Введите ru или en').trim().toLowerCase();
+'use strict'
 
-let daysru = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота'
-  ];
-  let daysen = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
-//через if
-if(lang==='ru'){
-    console.log(daysru);
-}
-else if(lang==='en'){
-    console.log(daysen);
+function getModifiedString(incomeString){
+if (typeof(incomeString) != 'string'){
+  return alert("Введена не строка");
 }
 else{
-    console.log('You typed wrong');
+let modifiedString = incomeString.trim();
+if(modifiedString.length>30){
+modifiedString = modifiedString.slice(0, 30) + "...";
 }
-//через switch
-switch(lang){
-    case "ru":
-        console.log(daysru);
-        break;
-    case "en":
-        console.log(daysen);
-        break;
-    default:
-        console.log('You typed wrong');
+return modifiedString;
 }
-  //через многомерный массив  
-let lang_days = [];
-lang_days['ru'] = [
-    'Воскресенье',
-    'Понедельник',
-    'Вторник',
-    'Среда',
-    'Четверг',
-    'Пятница',
-    'Суббота'
-  ];
-lang_days['en'] = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday'
-  ];
+}
 
-console.log(lang_days[lang]);
-
-//2е задание
-let namePerson = prompt('Введите имя').trim()
-console.log(namePerson == "Артем"?"Директор":namePerson=="Максим"?"преподпватель":"студент");
+let tryString = prompt("Введите строку");
+let resultString = getModifiedString(tryString);
+console.log(resultString);
