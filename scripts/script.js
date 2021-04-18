c26f4ade16eba9db10b6e2b07ba8a777e889d7c1
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-18 17:23:44 +0300

    выполнил 5 усложненное

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,26 @@
 'use strict'
 
-function getModifiedString(incomeString){
-if (typeof(incomeString) != 'string'){
-  return alert("Введена не строка");
+let myArr = [];
+for(let i=0; i<7; i++){
+  myArr[i] = +prompt("Введите число")
 }
-else{
-let modifiedString = incomeString.trim();
-if(modifiedString.length>30){
-modifiedString = modifiedString.slice(0, 30) + "...";
-}
-return modifiedString;
-}
+for(let i=0; i<myArr.length; i++){
+  if(myArr[i].toString().startsWith('2') || myArr[i].toString().startsWith('4')){
+    console.log(myArr[i]);
+  }
 }
 
-let tryString = prompt("Введите строку");
-let resultString = getModifiedString(tryString);
-console.log(resultString);
+
+
+let n = +prompt("до какого числа выводить простые числа?");
+
+for (let k = 2; k <= n; k++) {
+  let check = [1, 2];
+    for (let j = 2; j <= Math.sqrt(k); j++) {
+    check[j] = k%j;
+    }
+  if (check.indexOf(0) == -1) {
+    console.log(`Число ${k}. Имеет делители только 1 и ${k}`);
+  } 
+
+}
