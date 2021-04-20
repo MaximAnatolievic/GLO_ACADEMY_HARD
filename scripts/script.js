e5fac6a40030dd892bae37577fcabd45c6c4988a
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-04-21 02:10:39 +0300

    7е усложнённое

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,33 @@
 'use strict'
 
-let myArr = [];
-for(let i=0; i<7; i++){
-  myArr[i] = +prompt("Введите число")
-}
-for(let i=0; i<myArr.length; i++){
-  if(myArr[i].toString().startsWith('2') || myArr[i].toString().startsWith('4')){
-    console.log(myArr[i]);
+let week = [
+  "Воскресенье",
+  "Понедельник",
+  "Вторник",
+  "Среда",
+  "Четверг",
+  "Пятница",
+  "Суббота",
+];
+
+let day = new Date().getDay();
+
+console.log(day);
+
+function getDays(){
+  for(let i = 0; i<week.length; i++){
+    if(i>=1&&i<=5){
+    document.getElementById(i).innerHTML = week[i];
+    }
+    else{
+      document.getElementById(i).innerHTML = "<i>" + week[i] +"</i>";
+    }
+  };
+  if(day>=1&&day<=5){
+  document.getElementById(day).innerHTML = "<b>" + week[day] + "</b>";
+  }
+  else{
+    document.getElementById(day).innerHTML = "<b><i>" + week[day] + "</i></b>"
   }
 }
 
-
-
-let n = +prompt("до какого числа выводить простые числа?");
-
-for (let k = 2; k <= n; k++) {
-  let check = [1, 2];
-    for (let j = 2; j <= Math.sqrt(k); j++) {
-    check[j] = k%j;
-    }
-  if (check.indexOf(0) == -1) {
-    console.log(`Число ${k}. Имеет делители только 1 и ${k}`);
-  } 
-
-}
