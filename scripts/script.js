20bbc858585d2c798e35b3404fca5b50c4bad7f5
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-05-04 14:35:27 +0300

    в стиле класса по заданию

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,35 @@
 'use strict'
 
-const DomElement = function(selector, height, width, bg, fontSize, name){
-    this.elem = document.createElement('div');    
-    const inp = document.createElement('input');
-    this.elem.appendChild(inp);
-    inp.style.position = 'relative';
-    inp.placeholder = 'Введи текст';
-    this.selector = selector;
-    this.elem.style.height = height + 'px';
-    this.elem.style.width = width + 'px';
-    this.elem.style.backgroundColor = bg;
-    this.elem.style.fontSize = fontSize;
-    if(this.selector == '.'){
-        this.elem.className = name;
+class DomElement{
+    constructor(selector, height, width, bg, fontSize, name){
+        this.elem = document.createElement('div');    
+        const inp = document.createElement('input');
+        this.elem.appendChild(inp);
+        inp.style.position = 'relative';
+        inp.placeholder = 'Введи текст';
+        this.selector = selector;
+        this.elem.style.height = height + 'px';
+        this.elem.style.width = width + 'px';
+        this.elem.style.backgroundColor = bg;
+        this.elem.style.fontSize = fontSize;
+        if(this.selector == '.'){
+            this.elem.className = name;
+        }
+        if(this.selector == '#'){
+            this.elem.id = name;
+        }
+        this.elem.style.position='absolute';
+        this.elem.left = 0;
+        this.elem.top = 0;
+        inp.style.width = width*0.8 + 'px';
+        inp.style.left = 0.045*width +'px';
+        inp.style.top = height*0.3 + 'px';
+        inp.style.backgroundColor = bg;
     }
-    if(this.selector == '#'){
-        this.elem.id = name;
-    }
-    this.elem.style.position='absolute';
-    this.elem.left = 0;
-    this.elem.top = 0;
-    inp.style.width = width*0.8 + 'px';
-    inp.style.left = 0.045*width +'px';
-    inp.style.top = height*0.3 + 'px';
-    inp.style.backgroundColor = bg;
-    
-    
+    add (){
+        document.body.append(this.elem);
+    };
 
-};
-
-
-DomElement.prototype.add = function(){
-    document.body.append(this.elem);
 };
 
 let aaa = new DomElement('#', 100, 100, 'red', 50, 'sqr');
@@ -41,7 +39,7 @@ document.addEventListener("DOMContentLoaded", aaa.add());
 document.addEventListener('keydown', function shake(event){
     
     const cls =document.querySelector('div');
-    //cls.style.position = 'absolute';
+
            
     switch(event.code){
         case 'ArrowRight':
