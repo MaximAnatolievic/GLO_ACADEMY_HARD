8607e8256973714df9288ca2635b0d40e6a6c3c0
Maxim Ivanov <Maxim.Anatolievic@yandex.ru>
2021-05-04 17:51:51 +0300

    выполнил 15

---
+++ scripts/script copy.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class First{
+    hello(){
+        console.log('Привет я метод родителя!');
+    }
+}
+
+const first = new First();
+first.hello();
+
+class Second extends First{
+    hello(){
+        super.hello();
+        console.log('А я наследуемый метод!');
+    }
+};
+const second = new Second();
+second.hello();
+
